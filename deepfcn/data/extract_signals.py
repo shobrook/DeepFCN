b83dcbf9df58369f4af9ec720ef083e2cfed0f0d
Jonathan Shobrook <shobrookj@gmail.com>
2022-03-11 23:00:52 -0500

    Added docstring to extract_signals

--- deepfcn/data/extract_signals.py
+++ deepfcn/data/extract_signals.py
@@ -2,8 +2,25 @@ from nilearn import image
 
 
 def extract_signals(niimg, roi_masker):
+    """
+    Extracts BOLD signals (e.g. time-series) from each ROI in an fMRI scan (e.g.
+    Niimg).
+
+    Parameters
+    ----------
+    niimg : Niimg-like object, string
+        fMRI scan to extract signals from; if string, represents a path to a
+        NIfTI image
+    roi_masker : NiftiMasker object
+        ROI mask that tells nilearn which ROIs to extract signals from
+
+    Returns
+    -------
+    numpy.ndarray
+        Array of BOLD signals; shape = [num_rois, time_series_len]
+    """
     niimg = image.load_img(niimg) if isinstance(niimg, str) else niimg
     confounds = image.high_variance_confounds(niimg)
-    time_series = masker.fit_transform(niimg, confounds=confounds)
+    time_series = roi_masker.fit_transform(niimg, confounds=confounds)
 
     return time_series
