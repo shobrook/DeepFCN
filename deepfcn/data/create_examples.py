176a5a868fa7eccc4dd9013419d0452a679d5f86
Jonathan Shobrook <shobrookj@gmail.com>
2022-03-12 02:59:23 -0500

    Separated out Example object

--- deepfcn/data/create_examples.py
+++ deepfcn/data/create_examples.py
@@ -1,16 +1,6 @@
 from multiprocessing import cpu_count
 
 
-class Example(object):
-    def __init__(self, node_features, fc_matrix, y):
-        # TODO: Add data shape validations
-        self.node_features = node_features
-        self.fc_matrix, self.y = fc_matrix, y
-
-    def to_data_obj(self):
-        pass # TODO
-
-
-def create_examples(images, label, roi_masker, fc_measures=["correlation"],
+def create_examples(images, label, roi_masker, fc_features=["correlation"],
                     node_features=["mean"], n_jobs=cpu_count()):
     pass
