18b922ba011a32ab34122dbcca85bb3c753484c3
shobrook <shobrookj@gmail.com>
2022-07-29 03:44:04 -0400

    Updated drop_outliers docstring

--- deepfcn/data/drop_outliers.py
+++ deepfcn/data/drop_outliers.py
@@ -3,6 +3,19 @@ from outgraph import detect_outliers
 
 def drop_outliers(examples, cutoff=0.05):
     """
+    Converts FCNs into vectors and uses the Mahalanobis distance of each vector
+    from the distribution to detect and drop outliers.
+
+    Parameters
+    ----------
+    examples : list
+        list of Example objects representing the dataset to detect outliers in
+    cutoff : float
+        p-value threshold to use in determining which examples are outliers
+
+    Returns
+    -------
+    list : example set with outliers removed
     """
 
     graphs = [example.to_outgraph() for example in examples]
